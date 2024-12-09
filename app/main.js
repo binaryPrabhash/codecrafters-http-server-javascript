5053591c630847047ceb4dfc4cdc45836fb88d72
Prabhash Kumar <prabhash.kumar@indxtechnology.com>
2024-12-09 17:11:00 +0530

    read header attemp#2

--- app/main.js
+++ app/main.js
@@ -6,8 +6,14 @@ const server = net.createServer((socket) => {
 
     socket.on("data", (data) => {
         const path = data.toString().split(" ")[1]
-        const userAgent = data.toString().split("User-Agent: ")[1].split("\r")[0]
-        if (path === "/user-agent") {
+        const headers = data.toString().split("\r\n")
+        if (path === "/") {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n")
+        } else if (path.startsWith("/echo/")) {
+            const bodyText = path.split("/echo/")[1]
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${bodyText.length}\r\n\r\n${bodyText}`)
+        } else if (path === "/user-agent") {
+            const userAgent = headers[2].split("User-Agent: ")[1]
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
         } else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
