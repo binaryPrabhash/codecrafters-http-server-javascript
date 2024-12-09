f8df9390899ff7b47457f197e22f7eb6bd6393a4
Prabhash Kumar <prabhash.kumar@indxtechnology.com>
2024-12-09 14:57:27 +0530

    extract url

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ const server = net.createServer((socket) => {
     socket.on("data", (data) => {
         const path = data.toString().split(" ")[1]
 
-        const responseText = path === "/" ? "200 OK" : "404 NOT FOUND"
+        const responseText = path === "/" ? "200 OK" : "404 Not Found"
         socket.write(`HTTP/1.1 ${responseText}\r\n\r\n`)
     })
 
