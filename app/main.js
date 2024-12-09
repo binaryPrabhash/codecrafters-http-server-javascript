1fc089174f6ab3978a1bff2c9539f79d23579e42
Prabhash Kumar <prabhash.kumar@indxtechnology.com>
2024-12-09 17:58:50 +0530

    read file attempt#2

--- app/main.js
+++ app/main.js
@@ -1,23 +1,37 @@
 const net = require("net");
+const fs = require("fs")
+const path = require("node:path")
 
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
 
     socket.on("data", (data) => {
-        const path = data.toString().split(" ")[1]
+        const url = data.toString().split(" ")[1]
         const headers = data.toString().split("\r\n")
-        if (path === "/") {
+        if (url === "/") {
             socket.write("HTTP/1.1 200 OK\r\n\r\n")
-        } else if (path.startsWith("/echo/")) {
-            const bodyText = path.split("/echo/")[1]
+        } else if (url.startsWith("/echo/")) {
+            const bodyText = url.split("/echo/")[1]
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${bodyText.length}\r\n\r\n${bodyText}`)
-        } else if (path === "/user-agent") {
+        } else if (url === "/user-agent") {
             const userAgent = headers[2].split("User-Agent: ")[1]
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
+        } else if (url.startsWith("/files/")) {
+            const filename = url.split("/files/")[1]
+            const directory = process.argv[3]
+
+            if (fs.existsSync(`${directory}/${filename}`)) {
+                const content = fs.readFileSync(`${directory}/${filename}`).toString()
+                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
+
+            } else {
+                socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+            }
         } else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
         }
+        socket.end()
     })
 
     socket.on("close", () => {
